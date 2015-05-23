dbb65258e93730fef9d15ebf76c08878f818d547
Damian Kęska <webnull.www@gmail.com>
2015-05-23 12:57:07 +0200

    First steps in implementing compatibility layer with RainTPL3

---
+++ test/RainTPL4/CompatibilityLayerWithRainTPL3Test.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * RainTPL3 compatibility layer test
+ *
+ * @author Damian Kęska <damian@pantheraframework.org>
+ */
+class CompatibilityLayerWithRainTPL3Test extends RainTPLTestCase
+{
+    /**
+     * Set a STATIC (shared between instances) configuration key after instance was initialized
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testStaticConfigurationBeforeInstance()
+    {
+        $this->setup();
+        Rain\Tpl::configure('my-config-key', 'this is a test value');
+        $this->assertEquals('this is a test value', $this->engine->getConfigurationKey('my-config-key'));
+    }
+
+    /**
+     * Set a STATIC (shared between instances) configuration key BEFORE creating of instance
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testStaticConfigurationAfterInstance()
+    {
+        Rain\Tpl::configure('my-config-key', 'this is a test value');
+        $this->setup();
+        $this->assertEquals('this is a test value', $this->engine->getConfigurationKey('my-config-key'));
+    }
+}
