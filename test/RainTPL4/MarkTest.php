032e94ce8ff24711b5221fbfb0c32fdf97e65f6b
Mateusz Warzyński <lxnmen@gmail.com>
2015-05-25 16:41:50 +0200

    Implemented #4.

--- test/RainTPL4/MarkTest.php
+++ test/RainTPL4/MarkTest.php
@@ -7,7 +7,7 @@
 class MarkTest extends RainTPLTestCase
 {
     /**
-     * Testcase for nested foreach loop
+     * Testcase for goto -> mark
      *
      * <code>{goto test}This code should not execute.{mark test}mark works</code>
      * <expects>mark works</expects>
@@ -15,6 +15,34 @@ class MarkTest extends RainTPLTestCase
      * @author Mateusz Warzyński <lxnmen@gmail.com>
      */
     public function testGotoMark()
+    {
+        $this->setupRainTPL4();
+        $this->autoAssertEquals();
+    }
+
+    /**
+     * Testcase for tag {literal}
+     *
+     * <code>{literal}blablabla{/literal}something</code>
+     * <expects>something</expects>
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     */
+    public function testLiteral()
+    {
+        $this->setupRainTPL4();
+        $this->autoAssertEquals();
+    }
+
+    /**
+     * Testcase for tag {noparse}
+     *
+     * <code>{noparse}blablabla{/noparse}something</code>
+     * <expects>something</expects>
+     *
+     * @author Mateusz Warzyński <lxnmen@gmail.com>
+     */
+    public function testNoparse()
     {
         $this->setupRainTPL4();
         $this->autoAssertEquals();
